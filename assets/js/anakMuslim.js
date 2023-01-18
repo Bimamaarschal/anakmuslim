4fe1d5a980b4f82822e547a330d4810ba366d8cc
Bimamaarschal <bimamaarschal@gmail.com>
2023-01-19 00:30:25 +0700

    Bismillah

---
+++ assets/js/anakMuslim.js
@@ -0,0 +1,32 @@
+/*!
+=========================================================
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+Disesuiakan Oleh Bima Maarschal 
+Project anakMuslim
+=========================================================
+
+*/
+
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+});
